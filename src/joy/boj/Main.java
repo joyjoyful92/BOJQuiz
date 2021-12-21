1f43de62cd65b62e64c95cc155cfa76b84abdaae
joy <joy_joyful92@naver.com>
2021-12-21 14:10:36 +0900

    BOJ 문제 제출 형식 확인용 문제

--- src/joy/boj/Main.java
+++ src/joy/boj/Main.java
@@ -1,11 +1,15 @@
 package joy.boj;
 
-import joy.boj.quiz.BOJ1000;
+// BOJ 제출할 때,
+// 1. import copy
+// 2. 작성했던 class 는 Main class 안으로 복사
+// 3. 내부 class 가 static 인지 확인
+
+import joy.boj.quiz.BOJ1000; // test 했던 class import 는 제외하고 올릴 것
 
 public class Main {
-
     public static void main(String[] args) {
-        BOJ1000 quiz1000 = new BOJ1000();
-        quiz1000.runQuiz();
+        BOJ1000 answer = new BOJ1000();
+        answer.runQuiz();
     }
 }
--- src/joy/boj/quiz/BOJ1000.java
+++ src/joy/boj/quiz/BOJ1000.java
@@ -1,16 +1,16 @@
 package joy.boj.quiz;
 
+// A+B
+// 문제
+// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+// 입력
+// 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+// 출력
+// 첫째 줄에 A+B를 출력한다.
+
 import java.util.Scanner;
 
 public class BOJ1000 {
-    // A+B
-    // 문제
-    // 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
-    // 입력
-    // 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
-    // 출력
-    // 첫째 줄에 A+B를 출력한다.
-
     int A, B;
 
     public void runQuiz() {
@@ -19,11 +19,11 @@ public class BOJ1000 {
     }
 
     void dataScan() {
-        // 입력
+        // 입력, 문제에 없는 system.out 은 하지 말것
         Scanner sc = new Scanner(System.in);
-        System.out.println("값 입력");
         this.A = sc.nextInt();
         this.B = sc.nextInt();
+        sc.close(); // stream close 필수 -> runtime error 발생시킴
     }
 
     void getResult() {
