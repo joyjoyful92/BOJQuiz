61ac48c6778ce2249c75a2302eec6e6312bdbf91
joy <56821389+joyjoyful92@users.noreply.github.com>
2021-12-22 13:17:15 +0900

    boj1001 local setting test용 문제

--- src/joy/boj/Main.java
+++ src/joy/boj/Main.java
@@ -5,11 +5,13 @@ package joy.boj;
 // 2. 작성했던 class 는 Main class 안으로 복사
 // 3. 내부 class 가 static 인지 확인
 
-import joy.boj.quiz.BOJ1000; // test 했던 class import 는 제외하고 올릴 것
+// test 했던 class import 는 제외하고 올릴 것
+
+import joy.boj.quiz.BOJ1001;
 
 public class Main {
     public static void main(String[] args) {
-        BOJ1000 answer = new BOJ1000();
+        BOJ1001 answer = new BOJ1001();
         answer.runQuiz();
     }
 }
---
+++ src/joy/boj/quiz/BOJ1001.java
@@ -0,0 +1,32 @@
+package joy.boj.quiz;
+
+// A+B
+// 문제
+// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+// 입력
+// 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+// 출력
+// 첫째 줄에 A+B를 출력한다.
+
+import java.util.Scanner;
+
+public class BOJ1001 {
+    int A, B;
+
+    public void runQuiz() {
+        dataScan();
+        getResult();
+    }
+
+    void dataScan() {
+        // 입력, 문제에 없는 system.out 은 하지 말것
+        Scanner sc = new Scanner(System.in);
+        this.A = sc.nextInt();
+        this.B = sc.nextInt();
+        sc.close(); // stream close 필수 -> runtime error 발생시킴
+    }
+
+    void getResult() {
+        System.out.println(this.A - this.B);
+    }
+}
