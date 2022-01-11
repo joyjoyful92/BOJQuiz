205eaa1264d7ee2696215666b65cfc8b1f63d091
joy <56821389+joyjoyful92@users.noreply.github.com>
2022-01-11 23:51:16 +0900

    1043

--- src/joy/boj/Main.java
+++ src/joy/boj/Main.java
@@ -7,11 +7,11 @@ package joy.boj;
 
 // test 했던 class import 는 제외하고 올릴 것
 
-import joy.boj.quiz.BOJ1051;
+import joy.boj.quiz.BOJ1043;
 
 public class Main {
     public static void main(String[] args) {
-        BOJ1051 answer = new BOJ1051();
+        BOJ1043 answer = new BOJ1043();
         answer.runQuiz();
     }
 }
