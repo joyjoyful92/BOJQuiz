91253cc984b9a1360f8ea84dd439d6b4d06922da
joy <joy_joyful92@naver.com>
2022-01-25 10:49:31 +0900

    lv4 success

--- src/joy/boj/Main.java
+++ src/joy/boj/Main.java
@@ -7,11 +7,11 @@ package joy.boj;
 
 // test 했던 class import 는 제외하고 올릴 것
 
-import joy.boj.quiz.lv04.BOJ1110;
+import joy.boj.quiz.lv04.BOJ10951;
 
 public class Main {
     public static void main(String[] args) {
-        BOJ1110 answer = new BOJ1110();
+        BOJ10951 answer = new BOJ10951();
         answer.runQuiz();
     }
 }
--- src/joy/boj/quiz/lv04/BOJ10951.java
+++ src/joy/boj/quiz/lv04/BOJ10951.java
@@ -1,12 +1,12 @@
 package joy.boj.quiz.lv04;
 
 /*
-<A+B - 3>
+<A+B - 4>
 문제
 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
 
 입력
-첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+입력은 여러 개의 테스트 케이스로 이루어져 있다.
 
 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
 
@@ -18,16 +18,14 @@ import java.util.Scanner;
 
 public class BOJ10951 {
     Scanner sc;
-    int T, A, B;
+    int A, B;
 
     public void runQuiz() {
         sc = new Scanner(System.in);
-        this.T = sc.nextInt();
 
-        while (this.T > 0) {
+        while (sc.hasNext()) {
             dataScan();
             getResult();
-            this.T--;
         }
 
         sc.close(); // stream close 필수 -> runtime error 발생시킴
