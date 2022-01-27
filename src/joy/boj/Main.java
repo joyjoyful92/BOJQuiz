fed0fe173454414b2508642b7c74a7e6caa133ef
joy <joy_joyful92@naver.com>
2022-01-27 10:40:28 +0900

    lv5 success

--- src/joy/boj/Main.java
+++ src/joy/boj/Main.java
@@ -7,11 +7,11 @@ package joy.boj;
 
 // test 했던 class import 는 제외하고 올릴 것
 
-import joy.boj.quiz.lv05.BOJ3052;
+import joy.boj.quiz.lv05.BOJ1546;
 
 public class Main {
     public static void main(String[] args) {
-        BOJ3052 answer = new BOJ3052();
+        BOJ1546 answer = new BOJ1546();
         answer.runQuiz();
     }
 }
---
+++ src/joy/boj/quiz/lv05/BOJ1546.java
@@ -0,0 +1,56 @@
+package joy.boj.quiz.lv05;
+
+/*
+<평균>
+문제
+세준이는 기말고사를 망쳤다. 세준이는 점수를 조작해서 집에 가져가기로 했다. 일단 세준이는 자기 점수 중에 최댓값을 골랐다. 이 값을 M이라고 한다. 그리고 나서 모든 점수를 점수/M*100으로 고쳤다.
+
+예를 들어, 세준이의 최고점이 70이고, 수학점수가 50이었으면 수학점수는 50/70*100이 되어 71.43점이 된다.
+
+세준이의 성적을 위의 방법대로 새로 계산했을 때, 새로운 평균을 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 시험 본 과목의 개수 N이 주어진다. 이 값은 1000보다 작거나 같다. 둘째 줄에 세준이의 현재 성적이 주어진다. 이 값은 100보다 작거나 같은 음이 아닌 정수이고, 적어도 하나의 값은 0보다 크다.
+
+출력
+첫째 줄에 새로운 평균을 출력한다. 실제 정답과 출력값의 절대오차 또는 상대오차가 10-2 이하이면 정답이다.
+*/
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BOJ1546 {
+    Scanner sc;
+    int N;
+    int[] list;
+
+    public void runQuiz() {
+        sc = new Scanner(System.in);
+
+        dataScan();
+        getResult();
+
+        sc.close(); // stream close 필수 -> runtime error 발생시킴
+    }
+
+    void dataScan() {
+        this.N = sc.nextInt();
+        this.list = new int[this.N];
+
+        for ( int i = 0; i < this.N; i++ ) {
+            this.list[i] = sc.nextInt();
+        }
+    }
+
+    void getResult() {
+        Arrays.sort(this.list);
+        int max = this.list[this.N - 1];
+        double total = 0;
+
+        for ( int i = 0; i < this.N; i++ ) {
+            total += (double) this.list[i] / (double) max * 100;
+        }
+
+        System.out.println(total / this.N);
+    }
+}
