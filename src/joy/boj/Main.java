767a99137fa271e9ef27c5d6c011167fd368eaec
joy <56821389+joyjoyful92@users.noreply.github.com>
2022-02-02 17:12:03 +0900

    lv7 success

--- src/joy/boj/Main.java
+++ src/joy/boj/Main.java
@@ -7,11 +7,11 @@ package joy.boj;
 
 // test 했던 class import 는 제외하고 올릴 것
 
-import joy.boj.quiz.lv07.BOJ1152;
+import joy.boj.quiz.lv07.BOJ2908;
 
 public class Main {
     public static void main(String[] args) {
-        BOJ1152 answer = new BOJ1152();
+        BOJ2908 answer = new BOJ2908();
         answer.runQuiz();
     }
 }
