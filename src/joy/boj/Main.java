650dc0f3642c929142a5d468d86cb5c90553396f
joy <56821389+joyjoyful92@users.noreply.github.com>
2022-02-04 15:29:11 +0900

    lv8 success

--- src/joy/boj/Main.java
+++ src/joy/boj/Main.java
@@ -7,11 +7,11 @@ package joy.boj;
 
 // test 했던 class import 는 제외하고 올릴 것
 
-import joy.boj.quiz.lv08.BOJ1712;
+import joy.boj.quiz.lv08.BOJ2292;
 
 public class Main {
     public static void main(String[] args) {
-        BOJ1712 answer = new BOJ1712();
+        BOJ2292 answer = new BOJ2292();
         answer.runQuiz();
     }
 }
---
+++ src/joy/boj/quiz/lv08/BOJ2292.java
@@ -0,0 +1,48 @@
+ package joy.boj.quiz.lv08;
+
+/*
+<벌집>
+문제
+위의 그림과 같이 육각형으로 이루어진 벌집이 있다. 그림에서 보는 바와 같이 중앙의 방 1부터 시작해서 이웃하는 방에 돌아가면서 1씩 증가하는 번호를 주소로 매길 수 있다. 숫자 N이 주어졌을 때, 벌집의 중앙 1에서 N번 방까지 최소 개수의 방을 지나서 갈 때 몇 개의 방을 지나가는지(시작과 끝을 포함하여)를 계산하는 프로그램을 작성하시오. 예를 들면, 13까지는 3개, 58까지는 5개를 지난다.
+
+입력
+첫째 줄에 N(1 ≤ N ≤ 1,000,000,000)이 주어진다.
+
+출력
+입력으로 주어진 방까지 최소 개수의 방을 지나서 갈 때 몇 개의 방을 지나는지 출력한다.
+*/
+
+import java.util.Scanner;
+
+public class BOJ2292 {
+    Scanner sc;
+    int N;
+
+    public void runQuiz() {
+        sc = new Scanner(System.in);
+
+        dataScan();
+        getResult();
+
+        sc.close(); // stream close 필수 -> runtime error 발생시킴
+    }
+
+    void dataScan() {
+        this.N = sc.nextInt();
+    }
+
+    void getResult() {
+        int cnt = 1;
+
+        if ( this.N != 1 ) {
+           int bfrMax, curMax = 1;
+
+           while ( this.N > curMax ) {
+               bfrMax = curMax;
+               curMax = bfrMax + 6 * cnt++;
+           }
+        }
+
+        System.out.println(cnt);
+    }
+}
