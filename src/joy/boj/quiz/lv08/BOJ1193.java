d27b604d457f21ffbc6068159a5d93466ae3f735
joy <56821389+joyjoyful92@users.noreply.github.com>
2022-02-04 19:50:13 +0900

    lv8 ing

---
+++ src/joy/boj/quiz/lv08/BOJ1193.java
@@ -0,0 +1,58 @@
+ package joy.boj.quiz.lv08;
+
+/*
+<분수찾기>
+문제
+무한히 큰 배열에 다음과 같이 분수들이 적혀있다.
+
+1/1	1/2	1/3	1/4	1/5	…
+2/1	2/2	2/3	2/4	…	…
+3/1	3/2	3/3	…	…	…
+4/1	4/2	…	…	…	…
+5/1	…	…	…	…	…
+…	…	…	…	…	…
+이와 같이 나열된 분수들을 1/1 → 1/2 → 2/1 → 3/1 → 2/2 → … 과 같은 지그재그 순서로 차례대로 1번, 2번, 3번, 4번, 5번, … 분수라고 하자.
+
+X가 주어졌을 때, X번째 분수를 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 X(1 ≤ X ≤ 10,000,000)가 주어진다.
+
+출력
+첫째 줄에 분수를 출력한다.
+*/
+
+import java.util.Scanner;
+
+ public class BOJ1193 {
+     Scanner sc;
+     int N;
+
+     public void runQuiz() {
+         sc = new Scanner(System.in);
+
+         dataScan();
+         getResult();
+
+         sc.close(); // stream close 필수 -> runtime error 발생시킴
+     }
+
+     void dataScan() {
+         this.N = sc.nextInt();
+     }
+
+     void getResult() {
+         int cnt = 1;
+
+         if ( this.N != 1 ) {
+            int bfrMax, curMax = 1;
+
+            while ( this.N > curMax ) {
+                bfrMax = curMax;
+                curMax = bfrMax + cnt++;
+            }
+         }
+
+         System.out.println(cnt);
+     }
+ }
