81dc259c508c6f7c6ad2b9f5114b04a7bb3938f9
joy <56821389+joyjoyful92@users.noreply.github.com>
2022-02-18 21:30:09 +0900

    lv10 success - recursive

--- src/joy/boj/Main.java
+++ src/joy/boj/Main.java
@@ -7,11 +7,11 @@ package joy.boj;
 
 // test 했던 class import 는 제외하고 올릴 것
 
-import joy.boj.quiz.lv10.BOJ10870;
+import joy.boj.quiz.lv10.BOJ2447;
 
 public class Main {
     public static void main(String[] args) {
-        BOJ10870 answer = new BOJ10870();
+        BOJ2447 answer = new BOJ2447();
         answer.runQuiz();
     }
 }
